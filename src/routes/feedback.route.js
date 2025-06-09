f7825dff7095f94d5cde78aaca00ba372030726e
ngynphong <124499195+ngynphong@users.noreply.github.com>
2025-06-09 17:39:22 +0700

    add status in model feedback. addd function update fb status, get avg rating, get all fb

--- src/routes/feedback.route.js
+++ src/routes/feedback.route.js
@@ -1,21 +1,24 @@
 const express = require('express');
 const feedbackRouter = express.Router();
 const feedbackController = require('../controllers/feedback.controller');
-const { validateToken } = require('../middlewares/AuthMiddleware');
+const { validateToken, checkRole } = require('../middlewares/authMiddleware');
 // Tạo mới feedback
 feedbackRouter.post('/', validateToken, feedbackController.createFeedback);
 
 // Lấy tất cả feedback liên quan tới một user
-feedbackRouter.get('/user/:userId', validateToken, feedbackController.getFeedbackbyUser);
+feedbackRouter.get('/user/:id', validateToken, feedbackController.getFeedbackByUser);
 
 // Lấy feedback dành cho một coach
-feedbackRouter.get('/coach/:coachId', validateToken, feedbackController.getCoachFeedback);
+feedbackRouter.get('/coach/:id', validateToken, feedbackController.getCoachFeedback);
 
 // Xóa feedback
 feedbackRouter.delete('/:id', validateToken, feedbackController.deleteFeedback);
 // Cập nhật feedback
 feedbackRouter.put('/:id', validateToken, feedbackController.updateFeedback);
+feedbackRouter.put('/status/:id', validateToken, feedbackController.updateFeedbackStatus);
 
-feedbackRouter.get('/coach/:coachId/average-rating', validateToken, feedbackController.getCoachAverageRating);
+feedbackRouter.get('/', validateToken, checkRole(['admin']), feedbackController.getAllFeedback);
+feedbackRouter.get('/coach/:id/average-rating', validateToken, feedbackController.getCoachAverageRating);
+feedbackRouter.get('/system-rating', validateToken, feedbackController.getSystemAverageRating);
 
 module.exports = feedbackRouter;
